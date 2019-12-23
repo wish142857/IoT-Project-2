979b1acb5381e60d021ee4ec8dad25b5d06e8dfa
THU17cyz <congyezhen71@hotmail.com>
2019-12-23 16:01:08 +0800

    fmcw

---
+++ app/src/main/java/com/example/iot_project2/FMCW.java
@@ -0,0 +1,70 @@
+package com.example.iot_project2;
+
+public class FMCW {
+    private int fs;
+    private double T;
+    private int f0;
+    private int f1;
+    private int sample_num;
+    private double[] pseudo_T;
+    private int fft_len = Configuration.FMCW_FFTLen;
+    private int c = Configuration.SoundSpeed;
+
+
+    // 1-7 行
+    public FMCW(int sample_freq, double chirp_T, int start_freq, int end_freq) {
+        fs = sample_freq;
+        T = chirp_T;
+        f0 = start_freq;
+        f1 = end_freq;
+        sample_num = 1 + (int)(T * fs);
+        double[] t = new double[sample_num];
+        for (int i = 0; i < sample_num; i++) t[i] = i * ((double)1 / fs);
+        pseudo_T = Chirp.chirp(t, f0, T, f1);
+    }
+
+    public double delta_dis(double[] input) {
+        if (input.length != sample_num)
+            throw new RuntimeException("input data length wrong!");
+
+        // 点乘 计算FFT
+        Complex[] s = new Complex[fft_len];
+        for (int i = 0; i < fft_len && i < sample_num; i++)
+            s[i] = new Complex(pseudo_T[i] * input[i], 0);
+        for (int i = sample_num; i < fft_len; i++)
+            s[i] = new Complex(0, 0);
+
+        return cal_delta(s);
+    }
+
+
+    // 30-34 行
+    public double delta_dis(double[] input, int start) {
+        if (input.length - start < sample_num)
+            throw new RuntimeException("input data length wrong!");
+
+        Complex[] s = new Complex[fft_len];
+        for (int i = 0; i < fft_len && i < sample_num; i++)
+            s[i] = new Complex(pseudo_T[i] * input[start + i], 0);
+        for (int i = sample_num; i < fft_len; i++)
+            s[i] = new Complex(0, 0);
+
+        return cal_delta(s);
+    }
+
+    // 44-54 行
+    private double cal_delta(Complex[] s) {
+        // 计算频率偏移
+        Complex[] FFT_out = FastFourierTransform.fft(s);
+        double max_delta = 0;
+        int max_ind = -1;
+        for (int i = 0; i < fft_len / 2; i++){
+            double delta = FFT_out[i].abs();
+            if (delta > max_delta) {
+                max_delta = delta;
+                max_ind = i;
+            }
+        }
+        return max_ind * fs * c * T / (f1 - f0) / fft_len;
+    }
+}
---
+++ app/src/main/java/com/example/iot_project2/Receiver.java
@@ -0,0 +1,62 @@
+package com.example.iot_project2;
+
+public class Receiver {
+    private BandPassFilter bandPassFilter;
+    private FMCW fmcw;
+    private double[] filtered_sound;
+
+    public Receiver() {
+        bandPassFilter = new BandPassFilter(Configuration.BandPassCenter, Configuration.BandPassOffset, Configuration.SamplingRate);
+        fmcw = new FMCW(Configuration.SamplingRate, Configuration.T, Configuration.StartFreq, Configuration.EndFreq);
+    }
+
+    public void convert_and_filter(byte[] buffer, int byte_num) {
+        double[] doubles = new double[byte_num / 2];
+        for (int i = 0; i < doubles.length; i++) {
+            byte bl = buffer[2 * i];
+            byte bh = buffer[2 * i + 1];
+
+            short s = (short) ((bh & 0x00FF) << 8 | bl & 0x00FF);
+            doubles[i] = s / 32768f;
+        }
+        filtered_sound = bandPassFilter.filter(doubles);
+    }
+
+    public void convert_and_filter(byte[] buffer) {
+        convert_and_filter(buffer, buffer.length);
+    }
+
+    public double calculate_distance(int start_pos) {
+        return fmcw.delta_dis(filtered_sound, start_pos);
+    }
+
+    public static double[] xcorr(double[] input, double[] target) {
+        double[] xcorr_result = new double[input.length];
+        for (int i = 0; i < input.length; ++i) {
+            xcorr_result[i] = 0;
+            for (int j = 0; j < target.length && j < input.length - i; ++j) {
+                xcorr_result[i] += input[i + j] * target[j];
+            }
+        }
+        return xcorr_result;
+    }
+
+    public static int find_start_position(double[] input) {
+        double[] t = new double[Configuration.SampleNum];
+        for (int i = 0; i < Configuration.SampleNum; i++) t[i] = ((double)i / Configuration.SamplingRate);
+        double[] chirp = Chirp.chirp(t, Configuration.StartFreq, Configuration.T, Configuration.EndFreq);
+
+        double[] xcorr_result = xcorr(input, chirp);
+
+        double max = 0;
+        int pos = -1;
+        for (int i = 0; i < xcorr_result.length; i++) if (xcorr_result[i] > max) {
+            max = xcorr_result[i];
+            pos = i;
+        }
+
+        // Log.i("XCORR", String.format("max corr is: %.3f", max));
+        if (max > Configuration.StartThreshold && pos >= 20) return pos - 20;
+        else return -1;
+    }
+}
